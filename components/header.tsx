e47d7198bd58209975faa9dc96c6ee047493a209
MekkaLabs <labsmekka@gmail.com>
2023-08-07 10:33:39 -0300

    Adicionar Logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,16 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-        Blog
+        <Image
+          src="/logo-cannamed.png"
+          width={220}
+          height={60}
+          alt="Cannamed-tratamentos cannabicos"
+        />
       </Link>
       .
     </h2>
