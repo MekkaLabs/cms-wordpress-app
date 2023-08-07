ee8d33ec007ea960c148fe9f1d06dc4914ee63b1
MekkaLabs <labsmekka@gmail.com>
2023-08-07 10:52:24 -0300

    Atualizacao-Logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,10 @@
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-        <Image
-          src="/logo_cannamed.png"
-          width={220}
-          height={60}
-          alt="Cannamed-tratamentos cannabicos"
-        />
+        Blog
       </Link>
       .
     </h2>
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,10 +1,16 @@
 import { CMS_NAME, CMS_URL } from '../lib/constants'
+import Imagem from 'next/image'
 
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        <Image
+          src="/logo_cannamed.png"
+          width={220}
+          height={60}
+          alt="Cannamed Logo"
+        />
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{' '}
