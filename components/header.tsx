0840c7459ac875cfa6f64b90839f606e5d0c7d7f
MekkaLabs <labsmekka@gmail.com>
2023-08-07 10:38:32 -0300

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
         <Image
-          src="/logo-cannamed.png"
+          src="/logo_cannamed.png"
           width={220}
           height={60}
           alt="Cannamed-tratamentos cannabicos"
